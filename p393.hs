eab55ef9b5c3a3bffbbefcc3f975ce4a7241010b
Tevz <Tevz.Bajzelj@endava.com>
2022-09-08 17:12:46 +0200

    Improve 393

--- p393.hs
+++ p393.hs
@@ -1,14 +1,7 @@
 import Data.List
 
--- HELPER FUNCTION
-
 chunksOf _ [] = []
 chunksOf n l = (take n l):chunksOf n (drop n l)
-
--- function F splits each element of L into more elements then joins them
-split f l = concat $ map f l
-
--- NEIGHBOR CREATION
 
 rect0 n = neighbors
     where normal = chunksOf n [ x | x <- [1..(n * n)] ]
@@ -20,43 +13,24 @@ rect0 n = neighbors
 rect2 n = [1..n*n]
 rect n = zip (rect2 n) (rect0 n)
 
--- ACTUAL CODE
+each_square (i, e) (es_i, es) = (es_i, t1 es)
+    where t1 tes = t2 $ delete e tes
+          t2 tes = if es_i == e then delete i tes else tes
 
--- wrong_path l = any (\x -> length (ges x) == 0) l
---     where ges (_, es) = es
+each_chain (i, e) xs = map (each_square (i, e)) xs
 
-wrong_path [] = True
-wrong_path l = (length $ ges $ last l) == 0
-    where ges (_, es) = es
+each_option [] = []
+each_option (x:xs) = [ each_chain (hi, e) t | e <- hes ] ++ each_option xs
+    where (hi, hes) = head x
+          t = tail x
 
-neckit _ [] = []
-neckit _ ((_, []):_) = []
-neckit (i, e) ((xi, xes):xs) = (xi, t):(neckit (i, e) xs)
-    where t1 = xes \\ [e] -- transform 1: remove e
-          t2 = if xi == e then (t1 \\ [i]) else t1 -- transform 2: remove i
-          t = t2
+start l 0 = l
+start l i = start (each_option l) (i - 1)
 
--- [a] -> [[a]]
-perm nb = (map (\e -> neckit (i, e) other) es)
-    where first = head nb
-          other = tail nb
-          i = fst first
-          es = snd first
-
-each_l [] = []
-each_l (x:xs)
-    | wrong_path x = each_l xs
-    | otherwise = perm x ++ each_l xs
-
-begin l 0 = l
-begin l n = begin (each_l l) (n - 1)
-
--- func n = begin [l] 4
-func n = begin [l] (length l)
-    where l = rect n
-
--- 6m8.162s
--- f(6) = 207408
 -- NOT DONE
+-- f(6) = 207408 -> 5m32.868s
+func = length $ start [l] (length l)
+    where l = (rect 6)
+
 main = do
-    print $ (length $ func 6)
+    print func
