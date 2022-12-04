75c08008d35793965d4dc7f38fe6db99096af591
Tevz <tevz.8bajzelj@gmail.com>
2022-12-04 16:12:18 +0100

    aoc day 3 eval func improvment

--- aoc22/p03.hs
+++ aoc22/p03.hs
@@ -1,11 +1,18 @@
 import System.IO
+import Data.List
 import Data.List.Split
 import qualified Data.Set as Set
 
-eval c
-    | d >= fromEnum 'a' && d <= fromEnum 'z' = d - fromEnum 'a' + 1
-    | d >= fromEnum 'A' && d <= fromEnum 'Z' = d - fromEnum 'A' + 27
-        where d = fromEnum c
+-- original eval
+-- eval c
+--     | d >= fromEnum 'a' && d <= fromEnum 'z' = d - fromEnum 'a' + 1
+--     | d >= fromEnum 'A' && d <= fromEnum 'Z' = d - fromEnum 'A' + 27
+--         where d = fromEnum c
+
+-- eval with maybe
+eval c = case elemIndex c (['a'..'z'] ++ ['A'..'Z']) of
+    Just a -> a + 1
+    Nothing -> 0 -- should not happen the problem specifically says this is impossible
 
 el_1 l = eval $ head $ Set.toList $ Set.intersection (Set.fromList a) (Set.fromList b)
     where (a:b:[]) = chunksOf (div (length l) 2) l
@@ -23,4 +30,4 @@ func_2 ls = sum $ map el_2 (chunksOf 3 ls)
 main = do
     f <- openFile "data/p03.txt" ReadMode
     c <- hGetContents f
-    print $ func_2 $ lines c
+    print $ func_1 $ lines c
