a04e79696474f27baa7ea14f393285ccaf8055e1
user113333 <tevz.8bajzelj@gmail.com>
2024-10-25 20:13:01 +0200

    comments

--- eu/p070.cpp
+++ eu/p070.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 #define N 10000000
@@ -30,50 +31,34 @@ int main() {
 
     // sieve
     for (int p = 2; p < N; p++) {
-        if (fi[p] != 0) continue; // ni pra
+        if (fi[p] != 0) continue; // ni praštevilo
         pra.push_back(p);
         for (int i = 2; i * p < N; i++) {
             fi[i * p] = 1;
         }
 
-        // izracunaj fi za vsa pra stevila
+        // izračunaj fi za vsa praštevila
         // fi(p) = p - 1
         fi[p] = p - 1;
 
-        // izracunaj fi za vse potence pra stevila
+        // izračunaj fi za vse potence praštevil
         // fi(p^n) = p^(n-1) * (p - 1)
         for (int n = 2; round(pow(p, n)) < N; n++) {
             fi[(int)round(pow(p, n))] = round(pow(p, n - 1)) * (p - 1);
         }
     }
 
-    // izracunaj fi za vse potence pra stevil
-    // for (unsigned long long p : pra) {
-    //     for (int i = 2; round(pow(p, i)) < N; i++) {
-    //         unsigned long long pnai = round(pow(p, i));
-    //         // fi(n^m) = n^(m-1) * fi(n)
-    //         unsigned long long fi_pnai = round(pow(p, i-1)) * (p-1);
-    //         // tu lahko naredim fi(n * pnai) = fi(n) * fi(pnai)
-    //         // ker gcd(n, pnai) je enak 1
-    //         fi[pnai] = fi_pnai;
-    //     }
-    // }
-
-    auto other = [](unsigned long long p, unsigned long long i) {
-        unsigned long long pnai = round(pow(p, i));
-        // fi(n^m) = n^(m-1) * fi(n)
-        unsigned long long fi_pnai = round(pow(p, i-1)) * (p-1);
-        for (int n = 1; n * pnai < N; n++) {
-            if (n % p == 0) continue;
-            // tu lahko naredim fi(n * pnai) = fi(n) * fi(pnai)
-            // ker gcd(n, pnai) je enak 1
-            fi[n * pnai] = fi[n] * fi_pnai;
-        }
-    };
-
+    // izračunaj fi še za števila ki imajo več kot dva različna prafaktorja
     for (unsigned long long p : pra) {
         for (int i = 1; round(pow(p, i)) < N; i++) {
-            other(p, i);
+            unsigned long long pnai = round(pow(p, i));
+            unsigned long long fi_pnai = round(pow(p, i-1)) * (p-1); // fi(n^m) = n^(m-1) * fi(n)
+            for (int n = 1; n * pnai < N; n++) {
+                if (n % p == 0) continue; // gcd(n, pnai) mora biti 1
+                // tu lahko naredim fi(n * pnai) = fi(n) * fi(pnai)
+                // ker gcd(n, pnai) je enak 1
+                fi[n * pnai] = fi[n] * fi_pnai;
+            }
         }
     }
 
@@ -89,4 +74,5 @@ int main() {
     }
 
     cout << minimum_n << endl; // 8319823
+                               // yay :)
 }
